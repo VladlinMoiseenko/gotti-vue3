120311d8c68eae43e29fe76162cc0287eafcda0b
Vladlin Moiseenko <mvladlin@gmail.com>
2022-08-08 08:29:47 +0300

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container>
+  <h2>VueAxios demo</h2>
+  <ul>
+    <li v-for="user in users" :key="user.id">{{ user.name }}</li>
+  </ul>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      users: [],
+    };
+  },
+  async mounted() {
+    const { data } = await this.axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    this.users = data;
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
+import VueAxios from 'vue-axios'
+import axios from 'axios'
+
+loadFonts()
+
+createApp(App)
+  .use(vuetify)
+  .use(VueAxios, axios)
+  .mount('#app')

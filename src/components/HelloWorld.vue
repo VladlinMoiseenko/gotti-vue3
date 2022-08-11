1d54d24e5211c9d1f9f62911083ff155b962b475
Vladlin Moiseenko <mvladlin@gmail.com>
2022-08-11 13:42:04 +0300

    add lottie-player

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,77 @@
 <template>
-  <v-container>
-  <h2>VueAxios demo</h2>
-  <ul>
-    <li v-for="user in users" :key="user.id">{{ user.name }}</li>
-  </ul>
-  </v-container>
+
+  <div id="app">
+    <v-app id="inspire">
+
+      <v-content>
+
+       <v-container fluid>
+ 
+        <ul>
+          <li v-for="item in jsondata" :key="item.id" >
+            <lottie-player
+              autoplay
+              controls
+              loop
+              mode="normal"
+              :src=item.animationlottie
+              style="width: 320px"
+            >
+            </lottie-player>
+          </li>
+        </ul> 
+
+        </v-container> 
+
+      <v-container fluid>
+
+      </v-container> 
+
+      </v-content>
+      <v-footer></v-footer>
+    </v-app>
+  </div>
+
 </template>
 
 <script>
+import "@lottiefiles/lottie-player";
+
 export default {
+
   name: "App",
   data() {
     return {
-      users: [],
+      jsondata: [],
     };
   },
   async mounted() {
     const { data } = await this.axios.get(
-      "https://jsonplaceholder.typicode.com/users"
+      "http://gottiserv.vladlin.ru:3001/api/gotti"
     );
-    this.users = data;
-  },
-};
+    this.jsondata = data.data;
+    }, 
+  };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
 </style>
